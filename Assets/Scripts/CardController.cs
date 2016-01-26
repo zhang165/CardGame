24e170b5d2fcfef6c1557ff592022e6097d44c56
zhang165 <zhang165@gmail.com>
2016-01-26 13:15:51 -0800

    Simple card functionality

---
+++ Assets/Scripts/CardController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CardController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float left = Input.GetAxis ("Horizontal");
+		float top = Input.GetAxis ("Vertical");
+		transform.Rotate (new Vector3 (left, 0, top));
+	}
+}
