4909362dc14264ebeee43064c1af736f67089243
zhang165 <zhang165@gmail.com>
2016-01-26 16:51:05 -0800

    Buggy card dragging functionality

--- Assets/Scripts/CardController.cs
+++ Assets/Scripts/CardController.cs
@@ -2,10 +2,15 @@
 using System.Collections;
 
 public class CardController : MonoBehaviour {
+	Vector3 dist;
+	float posX;
+	float posY;
 
 	// Use this for initialization
 	void Start () {
-	
+		dist = Camera.main.WorldToScreenPoint (transform.position);
+		posX = Input.mousePosition.x - dist.x;
+		posY = Input.mousePosition.y - dist.y;
 	}
 	
 	// Update is called once per frame
@@ -13,5 +18,11 @@ public class CardController : MonoBehaviour {
 		float left = Input.GetAxis ("Horizontal");
 		float top = Input.GetAxis ("Vertical");
 		transform.Rotate (new Vector3 (left, 0, top));
+	}
+
+	void OnMouseDrag(){
+		Vector3 curPos = new Vector3 (Input.mousePosition.x - posX, Input.mousePosition.y - posY, dist.z);
+		Vector3 worldPos = Camera.main.ScreenToWorldPoint (curPos);
+		transform.position = worldPos;
 	}
 }
